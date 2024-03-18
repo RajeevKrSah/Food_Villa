b93f539f96aafaeda07fcfa4dae10e37e24408dc
RajeevKrSah <rajeevjnvkne@gmail.com>
2024-03-18 15:59:33 +0530

    IDE setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Home from "./pages/Home";
+
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
