cb88a53cc5ad97765286908870cded0d433ffa99
RajeevKrSah <rajeevjnvkne@gmail.com>
2024-03-18 22:44:07 +0530

    Routes are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-
+import Login from "./pages/Login";
 
 function App() {
   return (
     <>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className="flex justify-evenly">
+        <h2>FoodVilla</h2>
+        <ul className="flex">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
