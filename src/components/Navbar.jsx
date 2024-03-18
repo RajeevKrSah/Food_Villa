b7ee21c01498c15a2d597bd7359d980dd87bf115
RajeevKrSah <rajeevjnvkne@gmail.com>
2024-03-19 04:54:11 +0530

    Navbar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 function Navbar() {
   return (
-    <div className="flex justify-evenly">
-        <h1>FoodVilla</h1>
-        <ul className="flex">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-        </ul>
+    <div className="flex justify-evenly items-center h-16 bg-cyan-200">
+      <div className="flex gap-20 items-center">
+        <h1 className="text-3xl">FoodVilla</h1>
+        <p>
+          <Link to="/">Home</Link>
+        </p>
+      </div>
+
+      <ul className="flex">
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
     </div>
   );
 }
