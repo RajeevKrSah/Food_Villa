8fdc5e381bd7fcbee39ce9c5d94943830532085b
RajeevKrSah <rajeevjnvkne@gmail.com>
2024-03-24 15:14:55 +0530

    Carousel Component Added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Card from '../components/Card'
+import Carousel from '../components/Carousel'
 
 function Home() {
   return (
     <>
     <Navbar />
+    <Carousel />
     <Card />
     <Footer />
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,11 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    'node_modules/flowbite-react/lib/esm/**/*.js',
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('flowbite/plugin'),
-  ],
+  plugins: [],
 }
 
 
